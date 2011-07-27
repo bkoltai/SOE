c780e51e0feb6fdb590160ce3e3724057a703519
Benji Koltai <benjamin.koltai@tufts.edu>
2011-07-27 13:52:07 -0400

    Removed backup script

--- db/backupdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#first, delete any local backups older than 30 days
-#find backups/mysql -mtime +90 -type f | xargs rm -f
-#now dump the database into a gziped sql file
-/usr/local/mysql/bin/mysqldump -e -F --add-drop-table --create-options -q --single-transaction -u root -p soe_dev > /Users/BenjiKoltai/Desktop/Rails_Projects/NEW-soe-dev/db/backups/mysql/sql-backup-`date +%Y-%m-%d`.sql
