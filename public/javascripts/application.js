027b52bd4fd16cb5a2629c142a564b96dfd3c81f
Benji Koltai <benjamin.koltai@tufts.edu>
2011-07-27 22:45:47 -0400

    Show/Hide students added to researchers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,10 +4,22 @@ $(function(){
     $.getScript(this.href);
     return false;
   });
+  
+  $(".toggle_students").click(showStudents);
+  $("#projects th a, #researchers th a").live("click", function(){
+    $.getScript(this.href);
+    return false;
+  });
 });
 
-
-
 function showProjects(){
-  $(this).next().slideToggle();
+  $(this).siblings(".researcherProjects").slideToggle();
+  var text = $(this).text();
+  $(this).text(text == "Show Projects" ? "Hide Projects" : "Show Projects");
+}
+
+function showStudents(){
+  $(this).siblings(".researcherStudents").slideToggle();
+  var text = $(this).text();
+  $(this).text(text == "Show Students" ? "Hide Students" : "Show Students");
 }
